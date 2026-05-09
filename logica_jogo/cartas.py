bbdbc58aab95befeac0b6ed65c7ba388eba5f133
mariacfflores <mariaclarafflores@gmail.com>
2026-05-09 12:57:15 -0300

    feat: adiciona cartas de dragao no baralho

--- logica_jogo/cartas.py
+++ logica_jogo/cartas.py
@@ -3,9 +3,24 @@ import random
 
 TRIBES = ['Centauros', 'Elfos', 'Anões', 'Orcs', 'Magos', 'Trolls']
 REALMS = ['strath', 'pelia', 'nida', 'shetan', 'alara', 'fara']
+DRAGON_TRIBE = 'Dragao'
+DRAGON_REALM = 'Especial'
+DRAGON_COUNT = 3
 
 def generate_deck():
     # Cria o baralho base combinando tribos e reinos
-    deck = [{'tribe': t, 'realm': r} for t in TRIBES for r in REALMS]
+    deck = [
+        {'tribe': t, 'realm': r, 'is_dragon': False}
+        for t in TRIBES
+        for r in REALMS
+    ]
+    deck.extend(
+        {
+            'tribe': DRAGON_TRIBE,
+            'realm': DRAGON_REALM,
+            'is_dragon': True,
+        }
+        for _ in range(DRAGON_COUNT)
+    )
     random.shuffle(deck)
     return deck
